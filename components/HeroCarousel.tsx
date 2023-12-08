80b59cf5304ca0e1ea8d668ad9db5c5998f447e6
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-08 14:54:12 -0500

    🍰 (55:37)display bottom arrow and resize the Carousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -16,13 +16,17 @@ type THeroCarousel = {}
 
 const HeroCarousel = ({ }: THeroCarousel) => {
   return (
-    <Carousel showThumbs={false} autoPlay infiniteLoop interval={2000} showArrows={false} showStatus={false}>
-      {heroImages.map((image, idx) => {
-        return (
-          <Image src={image.imgUrl} alt={image.alt} key={idx} width={484} height={484} className="object-contain" />
-        )
-      })}
-    </Carousel>
+    <div className="hero-carousel">
+      <Carousel showThumbs={false} autoPlay infiniteLoop interval={2000} showArrows={false} showStatus={false}>
+        {heroImages.map((image, idx) => {
+          return (
+            <Image src={image.imgUrl} alt={image.alt} key={idx} width={484} height={484} className="object-contain" />
+          )
+        })}
+      </Carousel>
+
+      <Image src="assets/icons/hand-drawn-arrow.svg" alt="arrow" width={175} height={175} className="max-xl:hidden absolute -left-[15%] bottom-0 z-0" />
+    </div>
   )
 }
 
