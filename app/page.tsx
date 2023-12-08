933bb3bc782c1af0e4d5e5c5d0ed779d4a4be967
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-08 14:15:11 -0500

    🔍 (40:19) Add the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,42 @@
+import Image from "next/image";
 type THome = {}
 
-const Home = (props: THome) => {
+const Home = ({ }: THome) => {
   return (
-    <div>Home</div>
+    <>
+      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image src="/assets/icons/arrow-right.svg" alt="arrow-right" width={16} height={16} />
+            </p>
+            <h1 className="head-text">
+              Unleash the Power of{" "}
+              <span className="text-primary">PriceScraper</span>
+            </h1>
+            <p className="mt-6">
+              Empowering users with robust, self-service product and growth analytics to optimize conversion, foster engagement, and enhance customer retention.
+            </p>
+
+            Searchbar
+          </div>
+
+          HeroCarousel
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["Apple IPhone 15", "Book", "Sneakers"].map((product, idx) => {
+            return (
+              <div key={idx} className="product-card">{product}</div>
+            )
+          })}
+        </div>
+      </section>
+    </>
   )
 }
 
