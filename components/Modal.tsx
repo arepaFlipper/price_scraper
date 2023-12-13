1f13e0fe31a85901186e98878d3bcda8957e5382
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-13 07:56:05 -0500

    🔬 (3:12:44) Dev the Modal to submit alerts

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,17 +1,36 @@
 "use client"
 
-import { Fragment, useState } from "react";
+import { Fragment, useState, FormEvent, ChangeEvent } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
 
-type Props = {}
+type TModal = {}
 
-const Modal = (props: Props) => {
+const Modal = ({ }: TModal) => {
   const [isOpen, setIsOpen] = useState(true);
-  const { Panel, Title, Description } = Dialog;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    setIsSubmitting(false);
+    setEmail("");
+    closeModal();
+  };
+
+  const { Overlay } = Dialog;
+  const { Child } = Transition;
 
   const openModal = () => setIsOpen(true);
 
   const closeModal = () => setIsOpen(false);
+
+  const handle_input_change = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+
+  }
   return (
     <>
       <button type="button" className="btn" onClick={openModal}>
@@ -19,18 +38,40 @@ const Modal = (props: Props) => {
       </button>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" onClose={closeModal} className="dialog-container">
-          <Panel>
-            <Title>
-              This will permanently deactivate your account
-            </Title>
-            <p>
-              Are you sure you want to deactivate your account? All of your data.
-              Will be permanently removed. This action cannot be undone.
-            </p>
-            <button onClick={() => setIsOpen(false)}>Deactivate</button>
-            <button onClick={() => setIsOpen(false)}>Cancel</button>
-          </Panel>
+        <Dialog as="div" onClose={closeModal} className="dialog-container opacity-[95%]">
+          <div className="min-h-screen px-4 text-center">
+            <Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
+              <Overlay className="fixed inset-0" />
+            </Child>
+
+            <span className="inline-block h-screen align-middle" aria-hidden="true" />
+            <Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-95">
+              <div className="dialog-content">
+                <div className="flex flex-col">
+                  <div className="flex justify-between">
+                    <div className="p-3 border border-gray-200 rounded-10">
+                      <Image src="/assets/icons/logo.svg" alt="logo" width={28} height={28} />
+                    </div>
+
+                    <Image src="/assets/icons/x-close.svg" alt="close" width={24} height={24} className="cursor-pointer" onClick={closeModal} />
+                  </div>
+
+                  <h4 className="dialog-head_text">Stay updated with this product pricing alerts right in your inbox!</h4>
+
+                  <p className="text-sm text-gray-600 mt-2">Never miss a bargain again with these timely alerts!</p>
+                </div>
+
+                <form className="flex flex-col mt-5" onSubmit={handleSubmit}>
+                  <label htmlFor="email" className="text-sm font-medium text-gray-700">Email address</label>
+                  <div className="dialog-input_container">
+                    <Image src="/assets/icons/mail.svg" alt="mail" width={18} height={18} />
+                    <input required type="email" id="email" value={email} onChange={handle_input_change} placeholder="Enter your email address" className="dialog-input" />
+                  </div>
+                  <button type="submit" className="dialog-btn">{(isSubmitting) ? "Submitting..." : "Track"}</button>
+                </form>
+              </div>
+            </Child>
+          </div>
         </Dialog>
       </Transition>
     </>
